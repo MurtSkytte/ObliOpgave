0d6f0bf24ef6023a12ce0e80f602ff9f84e85d3f
MurtSkytte <150661688+MurtSkytte@users.noreply.github.com>
2024-10-10 18:17:37 +0200

    Add files via upload

---
+++ ObliOpgave1/TrophiesRepository.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObliOpgave1
+{
+    public class TrophiesRepository
+    {
+        private int _nextId = 6;
+
+        private List<Trophy> _trophies = new()
+        {
+            new Trophy(1, "Hotdog", 1992),
+            new Trophy(2, "Pingpong", 2000),
+            new Trophy(3, "Badminton", 2020),
+            new Trophy(4, "Ironman", 2005),
+            new Trophy(5, "Marathon", 1990)
+        };
+
+
+
+        public TrophiesRepository()
+        {
+        }
+
+        public IEnumerable<Trophy> Get(int? yearAfter= null, string? competitionIncludes = null, string? orderBy = null)
+        {
+            IEnumerable<Trophy> result = new List<Trophy>(_trophies);
+            if (yearAfter != null)
+            {
+                result = result.Where(t => t.Year > yearAfter);
+            }
+            if (competitionIncludes != null)
+            {
+                result = result.Where(t => t.Competition.Contains(competitionIncludes));
+            }
+
+            if (orderBy != null)
+            {
+                orderBy = orderBy.ToLower();
+                switch (orderBy)
+                {
+                    case "competition":
+                    case "competition_asc":
+                        result = result.OrderBy(t => t.Competition); 
+                        break;
+                    case "competition_desc":
+                        result = result.OrderByDescending(t => t.Competition);
+                        break;
+                    case "year":
+                    case "year_asc":
+                        result = result.OrderBy(t => t.Year);
+                        break;
+                    case "year_desc":
+                        result = result.OrderByDescending(t => t.Year);
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+            return result;
+        }
+        public Trophy? GetById(int id)
+        {
+            return _trophies.Find(trophy => trophy.Id == id);
+        }
+        public Trophy Add(Trophy trophy)
+        {
+            trophy.Validate();
+            trophy.Id = _nextId++;
+            _trophies.Add(trophy);
+            return trophy;
+        }
+        public Trophy Remove(int id)
+        {
+            Trophy? trophy = GetById(id);
+            if (trophy == null)
+            {
+                return null;
+            }
+            _trophies.Remove(trophy);
+            return trophy;
+        }
+        public Trophy? Update(int id, Trophy trophy)
+        {
+            trophy.Validate();
+            Trophy? existingTrophy = GetById(id);
+            if (existingTrophy == null)
+            {
+                return null;
+            }
+            existingTrophy.Competition = trophy.Competition;
+            existingTrophy.Year = trophy.Year;
+            return existingTrophy;
+        }
+    }
+}
---
+++ ObliOpgave1/Trophy.cs
@@ -0,0 +1,51 @@
+﻿namespace ObliOpgave1
+{
+    public class Trophy
+    {
+        public int Id { get; set; }
+        public string Competition { get; set; }
+        public int Year { get; set; }
+
+        public Trophy(int id, string competition, int year)
+        {
+            Id = id;
+            Competition = competition;
+            Year = year;
+        }
+
+        public Trophy()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Competition} {Year}";
+        }
+
+        public void ValidateCompition()
+        {
+            if (Competition == null)
+            {
+                throw new ArgumentNullException(nameof(Competition), "Competition cannot be null");
+            }
+            if (Competition.Length < 3)
+            {
+                throw new ArgumentException(nameof(Competition), "Competition cannot be shorter than 3");
+            }
+        }
+
+        public void ValidateYear()
+        {
+            if (Year > 2024 || Year < 1970)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Year), "Year must be between 1970 and 2024");
+            }
+        }
+
+        public void Validate()
+        {
+            ValidateCompition();
+            ValidateYear();
+        }
+    }
+}
